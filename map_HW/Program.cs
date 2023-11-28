9290dec683607a968379db14f06837e981fd3dfa
Andr <Andr@DESKTOP-6VJFTI6>
2023-11-28 20:25:41 +0700

    Добавьте файлы проекта.

---
+++ map_HW/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace map_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            map<int, string> myMap = new map<int, string>(1,"asdf");
+            myMap.Add(3, "qwe");
+            myMap.Add(3, "qwe");
+            myMap.Add(6, "qwe");
+            myMap.Add(2, "qwe");
+            myMap.Add(4, "qwe");
+            myMap.Add(12, "qwe");
+            Console.WriteLine(myMap);
+
+            foreach(var i in myMap)
+            {
+                Console.WriteLine(i);
+            }
+
+            Dictionary<int, int> asdf = new Dictionary<int, int>();
+            asdf.Add(1, 2);
+            asdf.Add(2, 2);
+            asdf.Add(4, 2);
+            asdf.Add(3, 2);
+            Console.WriteLine(asdf);
+
+        }
+    }
+}
---
+++ map_HW/map.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace map_HW
+{
+    internal class map<K, V> : IEnumerable<KeyValuePair<K, V>>
+    {
+        private K[] key;
+        private V[] value;
+        private const int STARTSIZE = 5;
+        private int size;
+        private int index = 0;
+
+
+
+        public map(K key, V value)
+        {
+            this.key = new K[STARTSIZE];
+            this.value = new V[STARTSIZE];
+            this.key[index] = key;
+            this.value[index] = value;
+            index++;
+            size = STARTSIZE;
+        }
+
+        public void Add(K key, V value)
+        {
+            if (index == size)
+            {
+                IncrasheSizeArr();
+            }
+            if (FindKey(key))
+            {
+                this.key[index] = key;
+                this.value[index] = value;
+            }
+            else
+            {
+                this.key[index] = key;
+                this.value[index++] = value;
+            }
+
+        }
+
+        private void IncrasheSizeArr()
+        {
+            K[] arrK = new K[size * 2];
+            V[] arrV = new V[size * 2];
+            for (int i = 0; i < key.Length; i++)
+            {
+                arrK[i] = key[i];
+                arrV[i] = value[i];
+            }
+            key = arrK;
+            value = arrV;
+        }
+
+        private bool FindKey(K key)
+        {
+            for (int i = 0; i < this.key.Length; i++)
+            {
+                if (this.key[i].Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < size; i++)
+            {
+                sb.Append(key[i]);
+                sb.Append(" ");
+                sb.Append(value[i]);
+                sb.Append('\n');
+            }
+
+            return sb.ToString();
+        }
+
+        //IEnumerable
+        /* public IEnumerator<K> GetEnumerator()
+         {
+             for (int i = 0; i < index; i++)
+             {
+                 yield return key[i];
+             }            
+         }*/
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                yield return new KeyValuePair<K, V>(key[i], value[i]);
+            }
+        }
+
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return key.GetEnumerator();
+        }
+
+        //IEnumerator
+
+    }
+}
